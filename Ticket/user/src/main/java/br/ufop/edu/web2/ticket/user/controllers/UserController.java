135afbe09c3ca9e132dd4dcb454abae1503b7a1c
Luccas Carneiro <luccas.carneiro@terceiroslinx.com.br>
2025-05-29 23:33:00 -0300

    Updates

--- Ticket/user/src/main/java/br/ufop/edu/web2/ticket/user/controllers/UserController.java
+++ Ticket/user/src/main/java/br/ufop/edu/web2/ticket/user/controllers/UserController.java
@@ -3,8 +3,11 @@ package br.ufop.edu.web2.ticket.user.controllers;
 import br.ufop.edu.web2.ticket.user.domain.UserDomain;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.View;
 
 @RestController
 public class UserController {
@@ -14,5 +17,14 @@ public class UserController {
         return ResponseEntity.ok("Hello There - Service is running");
     }
 
+    @GetMapping("/users/report")
+    public ResponseEntity<String> getAllUsers1() {
+        return ResponseEntity.ok("This is My Report");
+    }
+
+    @GetMapping("/users/all")
+    public ResponseEntity<String> getAllUsers2() {
+        return ResponseEntity.ok("All Users are running");
+    }
 
 }
